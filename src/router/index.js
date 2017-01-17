945d4e900695c17e7a8682d0e00fa702dab4d03d
Barbara Liau <bj.liau@gmail.com>
2017-01-17 13:11:42 -0700

    Add router and routes

---
+++ src/router/index.js
@@ -0,0 +1,64 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import SignupForm from '../views/SignupForm';
+import SignupSuccess from '../views/SignupSuccess';
+import PasswordReset from '../views/PasswordReset';
+import Dashboard from '../views/Dashboard';
+import Buckets from '../views/Buckets';
+import NewBucket from '../views/NewBucket';
+import EditBucket from '../views/EditBucket';
+import FileBucket from '../views/FileBucket';
+import ApiDocs from '../views/ApiDocs';
+import Support from '../views/Support';
+import Billing from '../views/Billing';
+import NotFound from '../views/NotFound';
+
+Vue.use(Router);
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    // Open Routes
+    { path: '/signup', component: SignupForm },
+    { path: '/signup-success', component: SignupSuccess },
+    { path: '/password-reset', component: PasswordReset },
+    // Routes requiring login
+    { path: '/dashboard',
+      component: Dashboard,
+      children: [
+        {
+          path: '/buckets',
+          component: Buckets
+        },
+        {
+          path: '/buckets/new',
+          component: NewBucket
+        },
+        {
+          path: '/buckets/:bucketId/edit',
+          component: EditBucket
+        },
+        {
+          path: '/bucket/:bucketId/files',
+          component: FileBucket
+        },
+        {
+          path: '/api-docs',
+          component: ApiDocs
+        },
+        {
+          path: '/support',
+          component: Support
+        },
+        {
+          path: '/billing',
+          component: Billing
+        }
+      ]
+    },
+    // Redirect routes
+    { path: '*', component: NotFound }
+  ]
+});
+
+export default router;
