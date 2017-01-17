d6839a093563d056c8f2d7496a2802e14f115055
Barbara Liau <bj.liau@gmail.com>
2017-01-17 13:50:57 -0700

    Remove assets/logo reference

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div> HAI </div>
     <router-view>
     </router-view>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
 export default {
   name: 'app',
   components: {
-    Hello
   }
 }
 </script>
