11c3ac2d114789a6ff86d9f2c68ef4d8c60fc3e6
Barbara Liau <bj.liau@gmail.com>
2017-01-17 13:42:00 -0700

    Reformat with comments

--- src/main.js
+++ src/main.js
@@ -1,11 +1,34 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './views/App';
+import Vuetify from 'vuetify';
+import * as filters from './filters';
+import store from './store';
+import router from './router';
+import { sync } from 'vuex-router-sync';
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+// sync the router with the vuex store.
+// this registers `store.state.route`
+sync(store, router);
+
+Vue.use(Vuetify);
+
+// register global utility filters.
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key]);
+});
+
+// create the app instance.
+// here we inject the router and store to all child components,
+// making them available everywhere as `this.$router` and `this.$store`.
+const app = new Vue({
+  router,
+  store,
+  ...App
+});
+
+// expose the app, the router and the store.
+// note we are not mounting the app here, since bootstrapping will be
+// different depending on whether we are in a browser or on the server.
+export { app, router, store };
