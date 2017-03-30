6d876938c3a489363c60ffb67a2a59286e67bd0e
Barbara Liau <bj.liau@gmail.com>
2017-03-30 11:31:02 -0600

    Make own component since it's used so much

---
+++ src/components/Sj-Go-Back-Btn.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <router-link :to="{ name: goTo }">
+    <button class="btn btn-block btn-transparent">
+      <span class="go-back">Go Back</span>
+    </button>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'sj-go-back-btn',
+
+  props: {
+    goTo: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+// TODO: Make the stupid Go Back have text-decoration none
+
+</style>
