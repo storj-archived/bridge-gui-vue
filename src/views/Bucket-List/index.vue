5c884a37151295f79c03ef59a631e5bb411f7708
Barbara Liau <bj.liau@gmail.com>
2017-03-30 11:36:10 -0600

    Make button change routes and update CSS

--- src/views/Bucket-List/index.vue
+++ src/views/Bucket-List/index.vue
@@ -1,28 +1,27 @@
 <template>
-  <section class="bucket-list">
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12">
-          <h1 class="title pull-left">Buckets</h1>
+  <section class="bucket-list container">
+    <div class="row">
+      <div class="col col-xs-12">
+        <h1 class="title pull-left">Buckets</h1>
+        <router-link :to="{ name: 'Create Bucket' }">
           <button class="btn btn-green btn-action pull-right">
-            Create Button
+              Create Bucket
           </button>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="table-responsive content">
-            <div v-if="loading">
-              Loading . . .
-            </div>
-            <Bucket-List-Items v-else :buckets="buckets">
-            </Bucket-List-Items>
-          </div>
-        </div>
+        </router-link>
       </div>
     </div>
 
+    <div class="row">
+      <div class="col col-xs-12">
+        <div class="table-responsive content">
+          <div v-if="loading">
+            Loading . . .
+          </div>
+          <Bucket-List-Items v-else :buckets="buckets">
+          </Bucket-List-Items>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -59,26 +58,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.title {
-	margin-top: 0.5em;
-    margin-bottom: 0.75em;
-}
 
 .btn-action {
   margin-top: 8px;
   padding: 12px 36px 10px;
-}
-
-@media (max-width: 480px) {
-	.btn-action {
-		padding-left: 10px;
-		padding-right: 10px;
-	}
-}
-
-body.page {
-  background: #f9f9f9;
-  padding-bottom: 3em;
 }
 
 .content {
@@ -91,5 +74,12 @@ body.page {
 
 .table-responsive.content {
   padding: 0;
+}
+
+@media (max-width: 480px) {
+	.btn-action {
+		padding-left: 10px;
+		padding-right: 10px;
+	}
 }
 </style>
