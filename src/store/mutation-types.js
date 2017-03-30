690299fcda9a1386981f0f767a1b1a6c453c3443
Barbara Liau <bj.liau@gmail.com>
2017-03-30 11:39:33 -0600

    Add SET_STORJ_INSTANCE constant

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,3 +1,4 @@
 export const SET_USER = 'SET_USER';
 export const SET_PRIVATE_KEY = 'SET_PRIVATE_KEY';
 export const CREATE_KEY_PAIR = 'CREATE_KEY_PAIR';
+export const SET_STORJ_INSTANCE = 'SET_STORJ_INSTANCE';
