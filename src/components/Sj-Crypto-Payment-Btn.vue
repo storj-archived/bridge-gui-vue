8cba24049925d6bf139d7607f446aa57977e3259
Barbara Liau <bj.liau@gmail.com>
2017-03-30 19:01:59 -0600

    Add custom crypto btn component

---
+++ src/components/Sj-Crypto-Payment-Btn.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="crypto-payment-btn">
+    <h6>
+      Interested in paying in BTC or SJCX? Contact us at
+      <a href="mailto:sales@storj.io"> sales@storj.io</a>!
+    </h6>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sj-crypto-payment-btn'
+};
+</script>
+
+<style lang="scss">
+.crypto-payment-btn {
+  margin-top: 1.75rem;
+}
+</style>
