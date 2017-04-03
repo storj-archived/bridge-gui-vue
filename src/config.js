c336e0b82142fc9b4399edb3306066631cf61891
Barbara Liau <bj.liau@gmail.com>
2017-04-03 09:10:15 -0600

    Add BRIDGE_URL and BILLING_URL

--- src/config.js
+++ src/config.js
@@ -2,4 +2,8 @@ module.exports = {
   // Don't need any of the config vars from bridge-gui because none of this
   // is server-side rendered
   // dev server is for hot reloading
+  BRIDGE_URL: process.env.NODE_ENV === 'development'
+    ? 'http://bridge-ssl-proxy' : 'https://api.storj.io',
+  BILLING_URL: process.env.NODE_ENV === 'development'
+    ? 'localhost:3000/graphql' : ''
 };
