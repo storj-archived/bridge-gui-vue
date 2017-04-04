a51874a9204f6c9a700b906871a860e9e8af7549
Barbara Liau <bj.liau@gmail.com>
2017-04-04 15:20:34 -0600

    Initial commit for HTML/CSS

---
+++ src/views/Signup/New-User-Banner.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div
+    class="alert alert-info text-center"
+    role="alert"
+    data-alert-cookie="alert_new_user"
+  >
+    <span>
+      <p><strong>New User Bonus!</strong></p>
+      <p>
+        Sign up now to unlock <b>one year of free
+        service</b> — up to 25GB of storage and bandwidth
+        every month!
+      </p>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'new-user-banner'
+};
+</script>
