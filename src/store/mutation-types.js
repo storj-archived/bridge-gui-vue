9bf9ac2558465aadbc7aa11ce169538455f92465
Barbara Liau <bj.liau@gmail.com>
2017-04-04 15:22:21 -0600

    Add CLEAR_USER and SET_BUCKETS mutation types

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,4 +1,6 @@
 export const SET_USER = 'SET_USER';
+export const CLEAR_USER = 'CLEAR_USER';
 export const SET_PRIVATE_KEY = 'SET_PRIVATE_KEY';
 export const CREATE_KEY_PAIR = 'CREATE_KEY_PAIR';
 export const SET_STORJ_INSTANCE = 'SET_STORJ_INSTANCE';
+export const SET_BUCKETS = 'SET_BUCKETS';
