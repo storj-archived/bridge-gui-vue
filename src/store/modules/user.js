010693f38135b57995e63e1c24022e5d9c017029
Barbara Liau <bj.liau@gmail.com>
2017-04-05 17:10:54 -0600

    Add CLEAR_USER mutation and createUser action

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,8 @@
 import * as types from '@/store/mutation-types';
-// import axios from 'axios';
-// import Promise from 'bluebird';
+import axios from 'axios';
+import Promise from 'bluebird';
+import config from '../../../config';
+import errors from 'storj-service-error-types';
 
 const state = {
   email: ''
@@ -13,11 +15,33 @@ const mutations = {
 
     // save user to localStorage
     window.localStorage.setItem('email', email);
+  },
+
+  [types.CLEAR_USER] (state) {
+    console.log('commiting CLEAR_USER');
+    state.email = '';
+
+    window.localStorage.removeItem('email');
   }
 };
 
 const actions = {
-
+  /**
+   * Creates a new Storj user
+   */
+  createUser ({ commit, state }, credentials) {
+    return new Promise((resolve, reject) => {
+      console.log('ACTION: createUser', credentials);
+      axios.post(config.app.BRIDGE_URL + '/users', credentials)
+        .then((result) => {
+          if (result.status < 400) {
+            commit(types.SET_USER, credentials.email);
+            return resolve();
+          }
+        })
+        .catch((err) => reject(new errors.InternalError(err)));
+    });
+  }
 };
 
 export default {
