a459b988889f8c6cd4878cf5bf9ebf39ad01d6d9
Barbara Liau <bj.liau@gmail.com>
2017-04-05 15:30:09 -0600

    Add CLEAR_STORJ_INSTANCE mutation

--- src/store/modules/storj-instance.js
+++ src/store/modules/storj-instance.js
@@ -1,3 +1,4 @@
+/* eslint no-undef: ["error", { "typeof": false }] */
 import * as types from '@/store/mutation-types';
 
 const state = {
@@ -6,7 +7,12 @@ const state = {
 
 const mutations = {
   [types.SET_STORJ_INSTANCE] (state, storj) {
+    console.log('SET_STORJ_INSTANCE', storj);
     state.instance = storj;
+  },
+
+  [types.CLEAR_STORJ_INSTANCE] (state) {
+    state.instance = null;
   }
 };
 
