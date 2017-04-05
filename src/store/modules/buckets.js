e502a7888e3dcf2c97b9db415dcfb8a2e97d6450
Barbara Liau <bj.liau@gmail.com>
2017-04-05 15:52:38 -0600

    Working getBuckets and createBucket

--- src/store/modules/buckets.js
+++ src/store/modules/buckets.js
@@ -20,41 +20,23 @@ const mutations = {
   },
 
   [types.SET_CURRENT_BUCKET] (state, bucket) {
+    console.log('set current bucket', bucket);
     state.currentBucket = bucket;
   }
 };
 
 const actions = {
-  getBuckets ({ commit, state, rootState }) {
+  getBuckets ({ commit, state, dispatch, rootState }) {
     return new Promise((resolve, reject) => {
-      const testBuckets = [{
-        id: 1,
-        name: 'hai',
-        storage: 0,
-        transfer: 0,
-        status: 'Active'
-      }, {
-        id: 2,
-        name: 'bucket two',
-        storage: 0,
-        transfer: 0,
-        status: 'Active'
-      }];
-      commit(types.SET_BUCKETS, testBuckets);
-
-      setTimeout(function () {
-        return resolve();
-      }, 2000);
-
-      // WORKS. COMMENTING OUT UNTIL STORJ AUTH IS GOOD
-      // const storj = rootState.storj.instance;
-      // storj.getBucketList(function (err, buckets) {
-      //   if (err) {
-      //     return reject(err);
-      //   }
-      //   commit(types.SET_BUCKETS, buckets);
-      //   return resolve();
-      // });
+      dispatch('keypairAuth').then((storj) => {
+        storj.getBucketList(function (err, buckets) {
+          if (err) {
+            return reject(err);
+          }
+          commit(types.SET_BUCKETS, buckets);
+          return resolve();
+        });
+      });
     });
   },
 
