005e62f99087ff46d981f6bf9f4ea2e37b85a856
Barbara Liau <bj.liau@gmail.com>
2017-04-06 00:17:18 -0600

    Add route guards to dashboard routes

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,7 @@ import BucketFiles from '@/views/Buckets/Bucket-Files';
 import Support from '@/views/Support';
 import Billing from '@/views/Billing';
 import Referrals from '@/views/Referrals';
+import { fromLocalStorage } from '@/utils';
 
 Vue.use(Router);
 
@@ -42,6 +43,7 @@ const router = new Router({
       redirect: '/dashboard/buckets',
       name: 'Dashboard',
       component: Dashboard,
+      meta: { requiresAuth: true },
       children: [
         {
           path: 'buckets',
@@ -102,6 +104,37 @@ const router = new Router({
       redirect: '/not-found'
     }
   ]
+});
+
+router.beforeEach((to, from, next) => {
+  console.log(to.matched, from.meta);
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    // this route requires auth. check if authenticated
+    console.log('auth');
+    const privateKey = fromLocalStorage('privateKey');
+
+    if (privateKey) {
+      next();
+    } else {
+      next({ path: '/login' });
+    }
+  } else {
+    next();
+  }
+  // if (to.matched.some(record => record.meta.requiresAuth)) {
+  //   // this route requires auth, check if logged in
+  //   // if not, redirect to login page.
+  //   if (!auth.loggedIn()) {
+  //     next({
+  //       path: '/login',
+  //       query: { redirect: to.fullPath }
+  //     })
+  //   } else {
+  //     next()
+  //   }
+  // } else {
+  //   next() // make sure to always call next()!
+  // }
 });
 
 export default router;
