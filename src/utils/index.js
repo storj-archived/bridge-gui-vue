94e40dd4699248db3f606c4d89f44a05346a0630
Barbara Liau <bj.liau@gmail.com>
2017-04-06 00:18:04 -0600

    Make fromLocalStorage utils method

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,15 @@ import crypto from 'crypto';
 export const sha256 = function (i) {
   return crypto.createHash('sha256').update(i).digest('hex');
 };
+
+/**
+ * Gets items from localStorage
+ * @param {String} item - name of item to retrieve
+ * @returns {String}
+ */
+export const fromLocalStorage = function (item) {
+  if (window && window.localStorage) {
+    return window.localStorage.getItem(item);
+  }
+  return '';
+};
