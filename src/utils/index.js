80eaa7c4319b2d623c5e522bfc9afa521e1d0e4d
Barbara Liau <bj.liau@gmail.com>
2017-04-10 14:48:28 -0600

    Add roundToGBAmount, setToTwoDecimalPlaces, and promoCodes util functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -38,3 +38,46 @@ export const getSum = function (arr, field) {
 
   return sum;
 };
+
+/**
+ * Prettifies the amount printed in billing history.
+ * Returns either a string that lets the user know that the amount that
+ * has been used is than 0.01 GB or rounds the amount to two decimal
+ * places.
+ * @param {Number} num - number in bytes or GB
+ * @param {String} type - optional - undefined or 'bytes'
+ */
+export function roundToGBAmount (num, type) {
+  const GB = 1000000000;
+
+  const numInGB = type === 'bytes' ? num / GB : num;
+  const modNum = setToTwoDecimalPlaces(numInGB);
+
+  // Checks to see if the amount is less than one cent
+  if (modNum.indexOf('0.00') === 0) {
+    const lessThanOneCent = '< 0.01';
+    return lessThanOneCent;
+  }
+  return `${modNum}`;
+};
+
+export function setToTwoDecimalPlaces (num) {
+  const roundedToTwoPlaces = Math.round(num * 100) / 100;
+  const setToTwoPlaces = roundedToTwoPlaces.toFixed(2);
+
+  return setToTwoPlaces;
+};
+
+export function promoCodes (code) {
+  switch (code) {
+    case 'referral-recipient':
+    case 'referral-sender':
+      return 'Referral';
+
+    case 'storj-event':
+      return 'Storj';
+
+    default:
+      return 'Storj';
+  }
+};
