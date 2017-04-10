bb5172747f083704caa10a33f268612ef3ce8ab1
Barbara Liau <bj.liau@gmail.com>
2017-04-10 13:50:30 -0600

    Remove exclamation mark

--- src/components/Sj-Crypto-Payment-Btn.vue
+++ src/components/Sj-Crypto-Payment-Btn.vue
@@ -2,7 +2,7 @@
   <div class="crypto-payment-btn">
     <h6>
       Interested in paying in BTC or SJCX? Contact us at
-      <a href="mailto:sales@storj.io"> sales@storj.io</a>!
+      <a href="mailto:sales@storj.io"> sales@storj.io</a>
     </h6>
   </div>
 </template>
