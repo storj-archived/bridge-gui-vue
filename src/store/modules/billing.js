ab47edbb480e4f0edc18ba9d33a3c46e44b2bce5
Barbara Liau <bj.liau@gmail.com>
2017-04-10 13:45:15 -0600

    Dummy credits/debits until billing is hooked up

--- src/store/modules/billing.js
+++ src/store/modules/billing.js
@@ -1,12 +1,65 @@
 const state = {
   credits: [{
     id: 1,
-    amount: 1000,
-    created: new Date()
+    paid_amount: 1000,
+    invoiced_amount: 1000,
+    created: new Date(),
+    user: 'a@a.com',
+    paid: true,
+    payment_processor: 'stripe',
+    type: 'automatic'
+  }, {
+    id: 2,
+    paid_amount: 0,
+    invoiced_amount: 1000,
+    created: new Date(),
+    user: 'a@a.com',
+    paid: false,
+    payment_processor: 'stripe',
+    type: 'automatic'
+  },
+  {
+    id: 3,
+    paid_amount: 2000,
+    invoiced_amount: 2000,
+    created: new Date(),
+    user: 'a@a.com',
+    paid: true,
+    payment_processor: 'stripe',
+    type: 'automatic'
+  }, {
+    id: 4,
+    promo_code: 'referral-recipient',
+    promo_amount: 1000,
+    promo_expires: new Date(),
+    created: new Date(),
+    type: 'automatic'
   }],
-  debits: [],
+  debits: [{
+    id: 1,
+    amount: 1000,
+    user: 'a@a.com',
+    created: new Date(),
+    type: 'storage',
+    bandwidth: 0,
+    storage: 1920321
+  }, {
+    id: 2,
+    amount: 2000,
+    user: 'a@a.com',
+    created: new Date(),
+    type: 'storage',
+    bandwidth: 0,
+    storage: 10321
+  }, {
+    id: 3,
+    amount: 1000,
+    user: 'a@a.com',
+    created: new Date(),
+    type: 'bandwidth',
+    bandwidth: 123123
+  }],
   usage: 0,
-  balance: 1000,
   storage: 2020,
   bandwidth: 123123
 };
