88d9bd7e4f95b7dd086b82b3936c16644708e3a3
Barbara Liau <bj.liau@gmail.com>
2017-04-12 17:07:53 -0600

    Add more documentation and exmaples, and stringify payload

--- src/api/index.js
+++ src/api/index.js
@@ -17,23 +17,53 @@ const billingRequest = axios.create({
 /**
  * Authenticates requests sent to Billing. Only keypair authentication
  * is currently allowed
- * @params {Object} options
+ * @params {Object} options - Options parameter passed to request
  * @params {String} options.method - HTTP method ('GET', 'POST', 'PUT')
  * @params {String} options.path - Path for HTTP request
- * @params {Object} options.payload - JSON ('POST' or 'PUT' requests) or params
- *  object ('GET' requests)
+ * @params {Object} options.params - for 'GET' requests
+ * @params {Object} options.json - for 'POST' and 'PUT' requests
+ *
+ * Example usage:
+ *  billingClient.request({
+ *    method: 'GET',
+ *    path: '/credits/dateRange',
+ *    params: { startDate: '', endDate: '' }
+ *  })
+ *
+ *  billingClient.request({
+ *    method: 'POST',
+ *    path: '/credits/create',
+ *    json: { user: '', type: '' }
+ *  })
+ *
  */
 class BillingClient {
   constructor () {
   }
 
-  // billingClient.request('GET', '/credits/dateRange', { startDate: '', endDate: '' })
-  request({ method, path, payload }) {
+  request(options) {
+    const implementedMethods = ['GET', 'PUT', 'POST'];
+
     return new Promise((resolve, reject) => {
       const privateKey = fromLocalStorage('privateKey');
       const publicKey = fromLocalStorage('publicKey');
 
-      if ()
+      if (!privateKey) {
+        return reject(new errors.NotAuthorizedError('Private key required'));
+      }
+
+      if (implementedMethods.indexOf(options.method) === -1) {
+        return reject(new errors.NotImplementedError('Method not implemented'));
+      }
+
+      // Stringify according to type of request
+      const payload = ['GET'].indexOf(options.method) !== -1
+        ? querystring.stringify(options.params)
+        : JSON.stringify(options.json);
+
+      // Create contract string in format of <METHOD>\<PATH>\n<PARAMS>
+      const contract = [options.method, options.path, payload].join('\n');
+
       const storj = new Storj({
         bridge: config.app.BRIDGE_URL,
         key: privateKey
