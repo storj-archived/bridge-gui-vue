8bb7192abf0ec287f00ef718210d4d55d43fac8e
Barbara Liau <bj.liau@gmail.com>
2017-04-17 12:38:09 -0600

    Update copy for referral

--- src/views/Referrals/Referral-Info.vue
+++ src/views/Referrals/Referral-Info.vue
@@ -2,10 +2,9 @@
   <div class="referral-info row text-center justify-content-center">
     <div class="col">
       <div class="spacer20"></div>
-      <h2>Give 50GB, Get 50GB</h2>
+      <h2>Give $10, Get $10</h2>
       <p>
-        Give your friends 50GB of storage and 50GB of bandwidth on Storj
-        every month for three months — all on us.
+        Give your friends a $10 credit to use on Storj — all on us.
         After they've spent $10 on Storj, we'll give you the same.
         There is no limit to how many friends you can refer.
       </p>
