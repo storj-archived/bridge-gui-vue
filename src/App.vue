972150ed2e72091f4c9da8e12aa53d4779166b70
Barbara Liau <bj.liau@gmail.com>
2017-04-21 14:08:39 -0600

    Fix messed up CSS for background

--- src/App.vue
+++ src/App.vue
@@ -31,38 +31,6 @@ export default {
 <style lang="scss">
 #main {
   font-family: 'Avenir Next LT Pro Regular';
-  height: 100%;
-  width: 100%;
   background-color: #f9f9f9;
-}
-
-.modal-scroll-body {
-  & > .modal-dialog {
-    height: 100%;
-    overflow-y: hidden;
-    margin: -40px auto;
-    top: 40px;
-    padding: 40px 0;
-
-    & > .modal-content {
-      height: 100%;
-      overflow: hidden;
-
-      & > .modal-body {
-        height: 100%;
-        overflow-y: scroll;
-        padding-bottom: 180px;
-        margin-bottom: -180px;
-        z-index: 1;
-        position: relative;
-      }
-
-      & > .modal-footer {
-        background: white;
-        z-index: 2;
-        position: relative;
-      }
-    }
-  }
 }
 </style>
