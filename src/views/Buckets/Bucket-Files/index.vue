27625e1a472df8fa0bcb7aaf3ca92b73b78722cc
Barbara Liau <bj.liau@gmail.com>
2017-04-21 18:03:10 -0600

    Add delete button, and require confirmation to delete bucket

--- src/views/Buckets/Bucket-Files/index.vue
+++ src/views/Buckets/Bucket-Files/index.vue
@@ -6,28 +6,60 @@
       </div>
     </div>
 
-    <div v-else class="row">
-      <div class="col col-xs-12">
+    <div v-else>
+      <div class="row">
+        <div class="header col col-sm-12">
+          <h1 class="title float-left">
+            Files in {{ bucket.name }}
+          </h1>
+          <!-- NB: Waiting for updateBucketById method in storj.js -->
+          <!-- <router-link :to="{
+            name: 'Bucket Settings',
+            params: { bucketId: $route.params.bucketId }
+          }">
+            <button class="btn btn-blue float-right">Edit Bucket</button>
+          </router-link> -->
 
-        <div class="row">
-          <div class="col col-sm-12">
-            <h1 class="title float-left">
-              Files in {{ bucket.name }}
-            </h1>
-            <!-- NB: Waiting for updateBucketById method in storj.js -->
-            <!-- <router-link :to="{
-              name: 'Bucket Settings',
-              params: { bucketId: $route.params.bucketId }
-            }">
-              <button class="btn btn-blue float-right">Edit Bucket</button>
-            </router-link> -->
-          </div>
+          <b-button
+            class="delete float-right"
+            variant="outline-danger"
+            v-b-modal="'deleteBucketModal'"
+          >
+            Delete Bucket
+          </b-button>
         </div>
 
-        <Bucket-File-List :files="files"></Bucket-File-List>
-        <Sj-Go-Back-Btn name="Buckets"></Sj-Go-Back-Btn>
+        <div v-show="error" class="col has-error"> {{ error }} </div>
       </div>
+
+      <Bucket-File-List :files="files"></Bucket-File-List>
+      <Sj-Go-Back-Btn name="Buckets"></Sj-Go-Back-Btn>
     </div>
+
+    <b-modal id="deleteBucketModal" title="Confirm Delete Bucket">
+      <div>
+        <p>
+          Are you sure you want to delete this bucket? This cannot be undone.
+        </p>
+
+        <p>Type in the bucket name <b>"{{ bucket.name }}"</b> to confirm deletion:
+        </p>
+
+        <b-form-input v-model="confirmBucketName"></b-form-input>
+      </div>
+
+      <footer slot="modal-footer">
+        <button
+          class="btn btn-danger"
+          @click.prevent="handleClick"
+          :disabled="confirmBucketName !== bucket.name"
+        >
+          <span v-show="!submitting">Yes, Delete Bucket</span>
+          <span v-show="submitting">Submitting . . .</span>
+        </button>
+      </footer>
+    </b-modal>
+
   </section>
 </template>
 
@@ -45,7 +77,6 @@ export default {
 
   created () {
     const bucketId = this.$route.params.bucketId;
-    console.log('bucketId', bucketId);
     const self = this;
     return Promise.join(
       this.getBucket(bucketId),
@@ -58,23 +89,51 @@ export default {
 
   data () {
     return {
-      loading: true
+      loading: true,
+      error: '',
+      confirmBucketName: '',
+      submitting: false
     };
   },
 
   methods: {
-    ...mapActions([ 'getBucket', 'getFileList' ])
+    ...mapActions([ 'getBucket', 'getFileList', 'deleteBucket' ]),
+
+    handleClick () {
+      this.error = '';
+      this.submitting = true;
+      this.deleteBucket(this.bucket.id)
+        .then(() => {
+          this.submitting = false;
+          this.$router.push({ name: 'Buckets' });
+        })
+        .catch((err) => {
+          this.submitting = false;
+          this.$root.$emit('hide::modal', 'deleteBucketModal');
+          this.error = err;
+        });
+    }
   },
 
-  computed: mapState({
-    bucket: state => state.buckets.current.meta,
-    files: state => state.buckets.current.files
-  })
+  computed: {
+    ...mapState({
+      bucket: state => state.buckets.current.meta,
+      files: state => state.buckets.current.files
+    })
+  }
 };
 </script>
 
-<style lang="scss" scoped>
-  .loading {
+<style lang="scss">
+  .bucket-files .loading {
     padding-top: 10rem;
+  }
+
+  .bucket-files .delete {
+    margin-top: 1.5rem;
+  }
+
+  .bucket-files .has-error {
+    margin-bottom: 1.5rem;
   }
 </style>
