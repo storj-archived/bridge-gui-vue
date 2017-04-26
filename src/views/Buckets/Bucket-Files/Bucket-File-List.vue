7221cfd8b1d5edc0b5ac8bc59607b636fbf7211a
Barbara Liau <bj.liau@gmail.com>
2017-04-26 16:45:15 -0600

    Fix empty bucket message

--- src/views/Buckets/Bucket-Files/Bucket-File-List.vue
+++ src/views/Buckets/Bucket-Files/Bucket-File-List.vue
@@ -1,14 +1,13 @@
 <template lang="html">
   <div class="row">
     <div class="col col-sm-12">
-      <div class="content table-responsive files-container">
+      <div v-if="!files.length" class="content text-center">
+        <span>Ready to store objects. Read the <a href="https://storj.readme.io/">Get Started</a> tutorial, or go straight to the <a href="https://storj.io/api.html">Storj API</a>.</span>
+      </div>
+
+      <div v-else class="content table-responsive files-container">
         <div class="form-group">
-
-          <div v-if="!files.length" class="content text-center">
-            No files in this bucket!
-          </div>
-
-          <table v-else class="table table-hover table-files">
+          <table class="table table-hover table-files">
             <thead>
               <tr>
                 <th>Name</th>
