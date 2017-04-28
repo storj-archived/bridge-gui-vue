23bbf00f7cc70e427eee1687e4f10a5c86eb6594
Barbara Liau <bj.liau@gmail.com>
2017-04-28 16:53:36 -0600

    Fix CSS and add classes

--- src/views/Password-Reset/index.vue
+++ src/views/Password-Reset/index.vue
@@ -2,64 +2,61 @@
   <section class="container password-reset">
     <Nav-Authentication></Nav-Authentication>
     <div class="container auth">
-      <div class="row">
-        <div class="col-lg-6 col-lg-push-3 col-md-8 col-md-push-2 col-xs-12 text-center">
-          <div class="row">
-            <div class="col-sm-12">
-              <div class="content">
-                <h1 class="title text-center form-group">
-                  Forgot your password?
-                </h1>
+      <div class="row justify-content-center">
+        <div class="col col-lg-6 col-lg-push-3 col-md-8 col-md-push-2 col-xs-12
+        text-center">
+          <div class="content">
+            <h1 class="title text-center form-group">
+              Forgot your password?
+            </h1>
 
-                <form>
+            <form>
 
-                  <p class="form-group">
-                    Enter your email address and a new password below, and we'll send you instructions on how to complete your password reset.
-                  </p>
+              <p class="form-group">
+                Enter your email address and a new password below, and we'll send you instructions on how to complete your password reset.
+              </p>
 
-                  <div class="form-group">
-                    <input
-                      type="email"
-                      class="form-control"
-                      name="email"
-                      placeholder="Email Address"
-                      v-model="email"
-                    />
-                  </div>
-
-                  <div class="form-group">
-                    <input
-                      type="password"
-                      class="form-control"
-                      name="password"
-                      placeholder="Password"
-                      v-model="password"
-                    />
-                  </div>
-
-                  <div class="form-group">
-                    <button
-                      type="submit"
-                      @click.prevent="submitPasswordReset"
-                      class="btn btn-block btn-green"
-                    >
-                      Reset My Password
-                    </button>
-                  </div>
-
-                  <div v-if="error">
-                    <span class="text-danger">{{ error }}</span>
-                  </div>
-
-                </form>
-
-                <p>
-                  Go back to
-                  <router-link :to="{ name: 'Login' }">Login</router-link>
-                </p>
-
+              <div class="form-group">
+                <input
+                  type="email"
+                  class="form-control email"
+                  name="email"
+                  placeholder="Email Address"
+                  v-model="email"
+                />
               </div>
-            </div>
+
+              <div class="form-group">
+                <input
+                  type="password"
+                  class="form-control password"
+                  name="password"
+                  placeholder="Password"
+                  v-model="password"
+                />
+              </div>
+
+              <div class="form-group">
+                <button
+                  type="submit"
+                  @click.prevent="submitPasswordReset"
+                  class="btn btn-block btn-green submit"
+                >
+                  Reset My Password
+                </button>
+              </div>
+
+              <div v-if="error">
+                <span class="text-danger">{{ error }}</span>
+              </div>
+
+            </form>
+
+            <p>
+              Go back to
+              <router-link :to="{ name: 'Login' }">Login</router-link>
+            </p>
+
           </div>
         </div>
       </div>
