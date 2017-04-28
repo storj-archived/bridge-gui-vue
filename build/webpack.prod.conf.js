6fb512682a9627fd208d9d1383babac1ad554778
Barbara Liau <bj.liau@gmail.com>
2017-04-27 21:35:35 -0600

    Add babili-webpack-plugin

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -8,6 +8,7 @@ var CopyWebpackPlugin = require('copy-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+var BabiliPlugin = require('babili-webpack-plugin')
 
 var env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -38,6 +39,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     //   },
     //   sourceMap: true
     // }),
+    // new BabiliPlugin({}),
     // extract css into its own file
     new ExtractTextPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css')
