04ee3e7ee7994669275a904ea399431011dc4c21
Barbara Liau <bj.liau@gmail.com>
2017-04-30 00:17:32 -0600

    Create component for same display of messages on auth pages

---
+++ src/components/Message-Page.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <section class="container auth">
+      <div class="row justify-content-center">
+        <div class="col text-center">
+          <div class="row">
+            <div class="col">
+              <div class="content form-horizontal">
+                <h1 class="title text-center form-group">{{ title }}</h1>
+                <div class="row">
+                  <p class="text-center">
+                    {{ message }}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'message-page',
+
+  props: {
+    title: String,
+    message: String
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
