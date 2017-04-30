4da9c47f35dfe1a0c79e6b6e8822c2804040e6f7
Barbara Liau <bj.liau@gmail.com>
2017-04-30 00:26:51 -0600

    Get rid of unnecessary divs and such

--- src/components/Message-Page.vue
+++ src/components/Message-Page.vue
@@ -1,20 +1,12 @@
 <template lang="html">
   <section class="container auth">
-      <div class="row justify-content-center">
-        <div class="col text-center">
-          <div class="row">
-            <div class="col">
-              <div class="content form-horizontal">
-                <h1 class="title text-center form-group">{{ title }}</h1>
-                <div class="row">
-                  <p class="text-center">
-                    {{ message }}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div class="content form-horizontal">
+      <h1 class="title text-center form-group">{{ title }}</h1>
+      <div class="row">
+        <p class="col text-center">
+          {{ message }}
+        </p>
+      </div>
       </div>
     </section>
 </template>
