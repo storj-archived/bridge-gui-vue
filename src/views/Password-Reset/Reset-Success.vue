af8bdec68b2bee0eb0e58953bd15513bc052f7d2
Barbara Liau <bj.liau@gmail.com>
2017-04-29 23:45:24 -0600

    Make success text

---
+++ src/views/Password-Reset/Reset-Success.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="content form-horizontal">
+    <h1 class="title text-center form-group">Password Reset Pending</h1>
+    <div class="row">
+      <p class="text-center">
+        Almost done! We just sent you a confirmation email.
+        Please follow the instructions to complete your password reset.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'reset-success'
+};
+</script>
+
+<style lang="scss">
+</style>
