fdafe5f0238de9ec7908136580a518bfb33eaf19
Dylan  Lott <lott.dylan@gmail.com>
2017-08-09 10:07:13 -0600

    added env variable logging

--- src/api/bridge-client.js
+++ src/api/bridge-client.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import config from '../../config';
 import Client from './client';
 
+console.log('bridge env var: ', process.env.BRIDGE_URL);
+
 const bridgeClient = new Client({
   baseURL: config.app.BRIDGE_URL,
   bridgeURL: config.app.BRIDGE_URL,
