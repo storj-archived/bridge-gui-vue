5262abd61b67ac1867b99a328c4e64ebd37965ad
Dylan  Lott <lott.dylan@gmail.com>
2017-08-09 10:02:02 -0600

    add a urls object for debugging

--- src/config.js
+++ src/config.js
@@ -2,6 +2,10 @@ module.exports = {
   // Don't need any of the config vars from bridge-gui because none of this
   // is server-side rendered
   // dev server is for hot reloading
+  URLS: {
+    BRIDGE: process.env.BRIDGE_URL,
+    BILLING: process.env.BILLING_URL
+  },
   BRIDGE_URL: process.env.NODE_ENV === 'development'
     ? 'http://bridge-ssl-proxy' : 'https://api.storj.io',
   BILLING_URL: process.env.NODE_ENV === 'development'
