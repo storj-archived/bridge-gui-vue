8381598bcf9243a12ac960b6193802d99667c64d
Dylan  Lott <lott.dylan@gmail.com>
2017-08-14 17:36:15 -0600

    Trying another hack

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ module.exports = {
   // Don't need any of the config vars from bridge-gui because none of this
   // is server-side rendered
   // dev server is for hot reloading
-  BRIDGE_URL: process.env.NODE_ENV === 'development'
-    ? 'http://bridge-ssl-proxy' : process.env.BRIDGE_URL,
-  BILLING_URL: process.env.NODE_ENV === 'development'
-    ? 'localhost:3000/graphql' : process.env.BILLING_URL
+  //BRIDGE_URL: process.env.NODE_ENV === 'development'
+  //  ? 'http://bridge-ssl-proxy' : process.env.BRIDGE_URL,
+  //BILLING_URL: process.env.NODE_ENV === 'development'
+  //  ? 'localhost:3000/graphql' : process.env.BILLING_URL
 };
