8c395011084b691bfcc2d22b38ba3d7c7e9f6d87
Dylan  Lott <lott.dylan@gmail.com>
2017-08-14 11:53:25 -0600

    Updating environment variables
    - Remove hardcoded URLs in billing and bridge client
    - Update client config to look at environment variables instead

--- config/index.js
+++ config/index.js
@@ -38,8 +38,8 @@ module.exports = {
   app: {
     NAME: 'storj-bridge-gui',
     BRIDGE_URL: process.env.NODE_ENV === 'development'
-      ? 'http://localhost:6382' : 'https://api.staging.storj.io',
+      ? 'http://localhost:6382' : process.env.BRIDGE_URL,
     BILLING_URL: process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000' : 'https://billing.staging.storj.io' 
+      ? 'http://localhost:3000' : process.env.BILLING_URL
   }
 }
