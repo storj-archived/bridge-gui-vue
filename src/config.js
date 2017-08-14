145ca78e1736e70da047d1a724a531b710e982be
Dylan  Lott <lott.dylan@gmail.com>
2017-08-14 11:48:45 -0600

    Remove unnecessary urls key

--- src/config.js
+++ src/config.js
@@ -2,10 +2,6 @@ module.exports = {
   // Don't need any of the config vars from bridge-gui because none of this
   // is server-side rendered
   // dev server is for hot reloading
-  URLS: {
-    BRIDGE: process.env.BRIDGE_URL,
-    BILLING: process.env.BILLING_URL
-  },
   BRIDGE_URL: process.env.NODE_ENV === 'development'
     ? 'http://bridge-ssl-proxy' : process.env.BRIDGE_URL,
   BILLING_URL: process.env.NODE_ENV === 'development'
