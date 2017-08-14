a62e97c628ad54e8edf1bc2b0d272d7131566f72
Philip Hutchins <flipture@gmail.com>
2017-08-14 15:34:30 -0400

    more logging

--- src/api/bridge-client.js
+++ src/api/bridge-client.js
@@ -3,6 +3,9 @@ import config from '../../config';
 import Client from './client';
 
 console.log('bridgeURL: ', config.app.BRIDGE_URL);
+
+console.log('config: ', config);
+console.log('process.env: ', process.env);
 console.log('bridgeURL process.env.BRIDGE_URL: ', process.env.BRIDGE_URL);
 console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
 
