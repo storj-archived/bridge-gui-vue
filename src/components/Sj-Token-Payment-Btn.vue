b3b65258c2c73f506cb37e8d4881b881a4721bd7
Alexander Leitner <leitnersalex@gmail.com>
2017-09-19 13:26:22 -0400

    Update to use coinpayments form

--- src/components/Sj-Token-Payment-Btn.vue
+++ src/components/Sj-Token-Payment-Btn.vue
@@ -1,15 +1,22 @@
 <template lang="html">
   <span class="token-payment-btn">
-    <b-button
-      href="https://bitpay.com/cart/add?itemId=JoY55YbAiBvdiV8EZVTmAw"
-      target="_blank"
-      class="btn btn-payment">
-        <img class="btn-payment-icon-storj-token"
-          :src="storjTokenIcon"
-          alt="storjToken"
-        />
-      <font class="btn-payment-text">STORJ</font>
-    </b-button>
+    <form class="token-payment-form" action="https://www.coinpayments.net/index.php" method="post">
+    	<input type="hidden" name="cmd" value="_pay">
+    	<input type="hidden" name="reset" value="1">
+    	<input type="hidden" name="merchant" value="d8b86a9501771dee3e5068f652121536">
+    	<input type="hidden" name="item_name" value="storj credit">
+    	<input type="hidden" name="currency" value="USD">
+    	<input type="hidden" name="amountf" value="5.00000000">
+    	<input type="hidden" name="quantity" value="1">
+    	<input type="hidden" name="allow_quantity" value="1">
+    	<input type="hidden" name="want_shipping" value="0">
+    	<input type="hidden" name="success_url" value="http://localhost:3001/dashboard/billing#">
+
+      <b-button target="_blank" class="btn btn-payment">
+        <img class="btn-payment-icon-storj-token" :src="storjTokenIcon" alt="STORJ token"/>
+        <font class="btn-payment-text">STORJ</font>
+      </b-button>
+    </form>
   </span>
 </template>
 
@@ -28,6 +35,11 @@ export default {
 </script>
 
 <style lang="scss">
+
+.token-payment-form {
+  display: inline;
+  margin: 0;
+}
 
 .token-payment-btn {
   margin-top: 1.75rem;
