50c8af585f537d10898fbed5276a46965d952dda
Dylan Lott <lott.dylan@gmail.com>
2018-02-27 14:00:43 -0700

    adding logging for debugging in staging

--- src/api/billing-client.js
+++ src/api/billing-client.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import config from '../../config';
 import Client from './client';
 
+console.log(`connecting to APIs ${config.app.BILLING_URL} ${config.app.BRIDGE_URL}`)
+
 const billingClient = new Client({
   baseURL: config.app.BILLING_URL,
   bridgeURL: config.app.BRIDGE_URL,
