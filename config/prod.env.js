852c11afd0fbbc5164afce65af785c253184061a
Dylan Lott <lott.dylan@gmail.com>
2018-02-27 14:20:10 -0700

    Update production envs with sane defaults at build time

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,8 @@
+console.log('building container with envs: ', process.env)
+
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: process.env.NODE_ENV || '"production"',
+  STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY || '"sk_test_W6L09JRZ1YR4Ua0KuDCDTST3"',
+  BILLING_URL: process.env.BILLING_URL || '"https://billing.staging.storj.io"',
+  BRIDGE_URL: process.env.BRIDGE_URL || '"https://api.staging.storj.io"'
 }
